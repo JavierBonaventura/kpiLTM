d3e2ff27ab0aa2cb5c449706eee016b00f125a4d
Javier Bonaventura <bonaventura.javier@gmail.com>
2023-11-17 11:19:35 +0100

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000/contacto',
+}));
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
