e98131f96815d1bcfc4f6ca29af95c94958703ad
Javier Bonaventura <bonaventura.javier@gmail.com>
2024-09-21 16:23:49 +0200

    primer commit

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// // Configuración de multer para la carga de archivos
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/'); // carpeta donde se guardarán los archivos
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.originalname); // nombre original del archivo
+//     }
+// });
+// const upload = multer({ storage: storage });
+
+
+//guardar archivo en memoria para usar vercel
 // Configuración de multer para la carga de archivos
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // carpeta donde se guardarán los archivos
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname); // nombre original del archivo
-    }
-});
+const storage = multer.memoryStorage(); // Almacenamiento en memoria
+
 const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
@@ -27,24 +34,41 @@ app.get('/', (req, res) => {
 });
 
 // Ruta para manejar la carga del archivo
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   try {
+//       const filePath = path.join(__dirname, 'uploads', req.file.filename);
+//       const workbook = XLSX.readFile(filePath);
+
+//       const jsonData = {};
+//       workbook.SheetNames.forEach(sheetName => {
+//           const worksheet = workbook.Sheets[sheetName];
+//           jsonData[sheetName] = XLSX.utils.sheet_to_json(worksheet); // Convertir cada hoja a JSON
+//       });
+
+//       res.render('resultado', { data: jsonData }); // Renderiza la vista resultado.ejs con los datos
+//   } catch (error) {
+//       console.error('Error al procesar el archivo:', error);
+//       res.status(500).send('Error al procesar el archivo.');
+//   }
+// });
+
+// Ruta para manejar la carga del archivo
 app.post('/upload', upload.single('file'), (req, res) => {
   try {
-      const filePath = path.join(__dirname, 'uploads', req.file.filename);
-      const workbook = XLSX.readFile(filePath);
+      const workbook = XLSX.read(req.file.buffer); // Lee desde el buffer en memoria
 
       const jsonData = {};
       workbook.SheetNames.forEach(sheetName => {
           const worksheet = workbook.Sheets[sheetName];
-          jsonData[sheetName] = XLSX.utils.sheet_to_json(worksheet); // Convertir cada hoja a JSON
+          jsonData[sheetName] = XLSX.utils.sheet_to_json(worksheet);
       });
 
-      res.render('resultado', { data: jsonData }); // Renderiza la vista resultado.ejs con los datos
+      res.render('resultado', { data: jsonData });
   } catch (error) {
       console.error('Error al procesar el archivo:', error);
       res.status(500).send('Error al procesar el archivo.');
   }
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
